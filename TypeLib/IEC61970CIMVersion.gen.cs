eba3015a09eeb96143e365ac4116dc2e943493d5
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-12-23 16:21:41 +1000

    Create IEC61970CIMVersion.gen.cs

---
+++ TypeLib/IEC61970CIMVersion.gen.cs
@@ -0,0 +1,34 @@
+using CimBios.Core.CimModel.DatatypeLib;
+using CimBios.Core.CimModel.DatatypeLib.OID;
+using CimBios.Core.CimModel.DatatypeLib.TypeLib;
+using CimBios.Core.CimModel.Schema;
+
+namespace CimBios.Gost58651.TypeLib;
+
+/// <summary>
+/// Номер версии IEC 61970 CIM, присвоенный этой модели UML.
+/// </summary>
+[CimClass(ClassUri)]
+public partial class IEC61970CIMVersion(IOIDDescriptor oid, ICimMetaClass metaClass)
+    : Resource(oid, metaClass)
+{
+    public new const string ClassUri = "http://iec.ch/TC57/CIM100#IEC61970CIMVersion";
+
+    /// <summary>
+    /// Форма - ГГГГ-ММ-ДД, например, для 5 января 2009 года это 2009-01-05.
+    /// </summary>
+    public DateTime? date
+    {
+        get => GetAttribute<DateTime?>(nameof(date));
+        set => SetAttribute(nameof(date), value);
+    }
+
+    /// <summary>
+    /// Форма - IEC61970CIMXXvYY, где XX - основная версия пакета CIM, а YY - младшая версия. Например, IEC61970CIM13v18.
+    /// </summary>
+    public string? version
+    {
+        get => GetAttribute<string?>(nameof(version));
+        set => SetAttribute(nameof(version), value);
+    }
+}
