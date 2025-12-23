63ef419269d2ab59b472b8c6017a80e4db0c12ab
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-12-23 16:57:45 +1000

    Create Gost58651Factory.cs

---
+++ Factory/Gost58651Factory.cs
@@ -0,0 +1,49 @@
+/*
+*    CimBios.Gost58651 - RF CIM Canonical model classes C# implementation.
+*    Copyright (C) 2025 Yuri A. Kovalenko a.k.a belizahrt <belizahrt@gmail.com>
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System.Reflection;
+using CimBios.Core.CimModel.DatatypeLib;
+using CimBios.Core.CimModel.DatatypeLib.Factory;
+using CimBios.Core.CimModel.Schema;
+using CimBios.Core.CimModel.Schema.RdfSchema;
+
+namespace CimBios.Gost58651.Factory;
+
+/// <summary>
+/// RF CIM Canonical model classes C# implementation.
+/// </summary>
+public class Gost58651Factory : ICimDatatypeLibFactory
+{
+    public ICimDatatypeLib Create()
+    {
+        var schema = new CimRdfSchemaXmlFactory().CreateSchema();
+        schema.Load(new StreamReader("Schemas/GOST_58651.rdfs"));
+
+        return Create(schema);
+    }
+
+    public ICimDatatypeLib Create(ICimSchema schema)
+    {
+        var thisAssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+
+        if (string.IsNullOrEmpty(thisAssemblyName))
+            throw new InvalidProgramException("Empty type lib assembly name!");
+        
+        return new CimDatatypeLib(thisAssemblyName, schema);
+    }
+}
